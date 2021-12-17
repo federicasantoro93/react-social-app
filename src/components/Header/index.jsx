e4f159fe9665f5355659793b19a3218a7f76584e
Federica Santoro <federica.santoro.1993@gmail.com>
2021-12-17 16:50:12 +0100

    feat: add router and app local state

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,18 @@
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 import styles from "./Header.module.scss";
 
 const Header = (props) => {
   const name = props.name || "App";
   const links = props.links || [{ link: "/", label: "Link" }];
 
+  const CheckActive = (link) => {
+    const resolved = useResolvedPath(link);
+    const match = useMatch({ path: resolved.pathname, end: true });
+  
+    return match ? styles.active : "";
+  };
+
+  
   return (
     <header className={styles.header}>
       <h1>{name}</h1>
@@ -11,7 +20,7 @@ const Header = (props) => {
         <ul>
           {links.map((item, index) => (
             <li key={index}>
-              <a href={item.link}>{item.label}</a>
+              <Link className={CheckActive(item.link)} to={item.link}>{item.label}</Link>
             </li>
           ))}
         </ul>
